9680e09a1a58a1934907f131a675d7fac8502f7a
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-15 19:38:11 -0300

    Update Potencia.hs

--- Potencia.hs
+++ Potencia.hs
@@ -1,3 +1,3 @@
 potencia :: Int -> Int -> Int
-potencia _ 0 = 1
-potencia x y = x * potencia x (y-1)
+potencia _ 0 = 1 --A potência de qualquer número elevado a 0 é 1
+potencia x y = x * potencia x (y-1) --Definição recursiva da potência
