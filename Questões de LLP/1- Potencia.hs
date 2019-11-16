2d6ac347edb2d0d216c1852130eda435d89a83cd
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-15 23:48:46 -0300

    Update 1- Potencia.hs

--- Questões de LLP/1- Potencia.hs
+++ Questões de LLP/1- Potencia.hs
@@ -1,3 +1,8 @@
-potencia :: Int -> Int -> Int
-potencia _ 0 = 1
-potencia x y = x * potencia x (y-1)
+{-
+Autor: Pierre Vieira
+Data: 15/11/2019
+Github: https://github.com/PierreVieira
+-}
+potencia :: Int -> Int -> Int -- Recebe 2 parâmetros inteiros e retorna um parâmetro inteiro tal que o primeiro parâmetro será elevado em potência pelo segundo
+potencia _ 0 = 1 -- Recebendo qualquer parâmetro inteiro e 0 o resultado será 1
+potencia x y = x * potencia x (y-1) -- Recebendo x y retorna o valor de x elevado à potência de y-1
