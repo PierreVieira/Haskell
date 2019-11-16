8ebacc3ee3703a3faa5414f0c80bcb06d7bdb63d
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-15 23:50:46 -0300

    Update 2- SomaImpares.hs

--- Questões de LLP/2- SomaImpares.hs
+++ Questões de LLP/2- SomaImpares.hs
@@ -1,5 +1,10 @@
-somaImpar :: [Int] -> Int
-somaImpar [] = 0
+{-
+Autor: Pierre Vieira
+Data: 15/11/2019
+Github: https://github.com/PierreVieira
+-}
+somaImpar :: [Int] -> Int -- Recebe uma lista de inteiros e retorna um inteiro
+somaImpar [] = 0 -- Se a lista está vaiza o resultado é 0
 somaImpar (x:xs)
-	| (x `mod` 2 == 1) = x + somaImpar xs
-	| otherwise = 0 + somaImpar xs
+	| (x `mod` 2 == 1) = x + somaImpar xs -- caso a cabeça seja par, retorne a cabeça + a soma dos ímpares corpo
+	| otherwise = 0 + somaImpar xs -- caso a cabeça seja ímpar, retorne a soma dos ímpares corpo
