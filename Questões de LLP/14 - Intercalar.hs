391d750f585a4806ba993341c696a9b4cf030ba3
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-16 15:12:15 -0300

    Add files via upload

---
+++ Questões de LLP/14 - Intercalar.hs
@@ -0,0 +1,11 @@
+{-
+	Autor: Pierre Vieira
+	Data: 15/11/2019
+	Github: https://github.com/PierreVieira
+-}
+intercalar :: [t] -> [t] -> [t]
+{-
+	Entradas: 2 listas
+	Saída: Listas intercaladas	
+-}
+intercalar lista1 lista2 = lista1++lista2
