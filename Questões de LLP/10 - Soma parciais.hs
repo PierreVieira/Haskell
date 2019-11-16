93bbf3d5ca3f1f2c95eb2d0c66b1dd4e43de5ceb
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-16 15:04:38 -0300

    Add files via upload

---
+++ Questões de LLP/10 - Soma parciais.hs
@@ -0,0 +1,20 @@
+{-
+	Autor: Pierre Vieira
+	Data: 15/11/2019
+	Github: https://github.com/PierreVieira
+-}
+progressaoAritmetica :: Int -> Int
+{-
+	Entrada: número inteiro
+	Saída número inteiro com a soma de 1 até n variando de 1 em 1	
+-}
+progressaoAritmetica 1 = 1
+progressaoAritmetica n = progressaoAritmetica(n-1) + n
+
+somaParciais :: [Int] -> [Int]
+{-
+	Entrada: lista de números inteiros
+	Saída: lista com as somas parciais de cada elemento da lista original
+-}
+somaParciais [] = [] -- Se a entrada for uma lista vazia, então a saída é uma lista vazia
+somaParciais (x:xs) = [progressaoAritmetica x]++somaParciais xs -- Conctena o resultado (em formato de lista) da progressão aritmética de um elemento com a chamada recursiva passando corpo como parâmetro
