725a56e8f3e329205899b289d551a2c1a57910db
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-16 21:14:21 -0300

    Update 11 - Linearizar.hs

--- Questões de LLP/11 - Linearizar.hs
+++ Questões de LLP/11 - Linearizar.hs
@@ -8,5 +8,5 @@ linearizar :: [[t]] -> [t]
 	Entrada: Lista de lista de termos
 	Saida: lista com os termos
 -}
-linearizar [] = []
-linearizar (x:xs) = x++linearizar(xs)
+linearizar [] = [] -- Se a lista está vazia retorne uma lista vazia
+linearizar (x:xs) = x++linearizar(xs) --Senão, concatena a cabeça com a linearização do corpo
