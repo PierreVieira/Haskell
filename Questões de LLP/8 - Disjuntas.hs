efb7b9e14bf02704e1fdad0127659d23d6ac84e6
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-18 18:35:53 -0300

    Update 8 - Disjuntas.hs

--- Questões de LLP/8 - Disjuntas.hs
+++ Questões de LLP/8 - Disjuntas.hs
@@ -4,15 +4,26 @@
 	Github: https://github.com/PierreVieira
 -}
 
+temNaLista :: Int -> [Int] -> Bool
+{-
+	Primeiro parâmetro: Inteiro a ser pesquisado em uma lista
+	Segundo parâmetor: Lista de inteiros em que será realizada a pesquisa
+	Saída: booleano dizendo se o número está na lista ou não
+-}
+temNaLista a [] = False -- Se estou comparando um valor 'a' com uma lista vazia, então esse valor não está na lista
+temNaLista a (x:xs) = (x == a) || temNaLista a xs -- Um valor 'a' está presente em uma lista se a cabeça dessa lista for igual a 'a' ou se 'a' está presente no corpo
+
+distintos :: [Int] -> Bool
+{-
+	Parâmetro de entrada: lista de números inteiros
+	Saída: booleano dizendo se a lista só possui números diferentes ou não	
+-}
+distintos [] = True -- Se a lista está vazia, então ela só possui elementos distintos
+distintos (x:xs) = if temNaLista x xs then False else distintos xs -- Se a cabeça está presente no corpo de uma lista então, a lista não é distina, senão ela é distina 
+
 disjuntas :: [Int] -> [Int] -> Bool
 {-
-	Primeiro parâmetro: lista de inteiros;
-	Segundo parâmetro: lista de inteiros;
-	Saída: booleano dizendo se as listas são diferentes ou não. 	
+	Parâmetros de entrada: 2 listas de números inteiros
+	Saída: booleano dizendo se as listas são disjuntas	
 -}
-disjuntas [] [] = False -- Se as duas listas estão vazias, então elas não são diferentes.
-disjuntas [] _ = True -- Se a primeira lista está vazia e a segunda não, então elas são diferentes.
-disjuntas _ [] = True -- Se a primeira lista não está vazia e a segunda está, então elas são diferentes. 
-disjuntas (a:b) (c:d) 
-	| (a==c) = disjuntas b d -- Compara a cabeça e chama a função se as cabeças forem iguais
-	| otherwise = True -- Se as cabeças forem diferentes retorna True
+disjuntas l1 l2 = distintos(l1++l2) --Passa como argumento a concatenação das duas listas e verifica se elas são iguais
